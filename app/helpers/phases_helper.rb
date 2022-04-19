bb400758b56fe0aef2c78696114cce293ab8510a
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-04-19 23:03:11 +0530

    Eager load data

--- app/helpers/phases_helper.rb
+++ app/helpers/phases_helper.rb
@@ -1,4 +1,5 @@
 module PhasesHelper
+
   def phase_payment_status_badge(phase)
     case phase.payment_status
     when "Paid"
@@ -11,4 +12,15 @@ module PhasesHelper
       "bg-warning"
     end
   end
+
+  def phase_completion_badge(phase)
+    if phase.percentage_completed_days < 50
+      "bg-warning"
+    elsif phase.percentage_completed_days >= 50 && phase.percentage_completed_days < 100
+      "bg-info"
+    else
+      "bg-success"
+    end
+  end
+
 end

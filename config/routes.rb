14e85f21f61c4b328ece2d8400d6aa13fa939fcb
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-05-02 19:46:03 +0530

    Added form dividers for project

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "projects#index"
+  root "projects#index", status: "In Progress"
 
   require 'sidekiq/web'
   authenticate :user do
